dd33cee95f5632a3a70d206953da9e3c70ce6cdd
Sosna123 <olek.arez@gmail.com>
2025-01-19 17:52:18 +0100

    made reverse direction possible

--- src/components/ShowStops.vue
+++ src/components/ShowStops.vue
@@ -1,22 +1,34 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import { busLineId } from "../dataObjs/arrays.ts";
-let dataGot = ref<string>("");
-async function fetchData(lineName: number) {
+let stopsArr = ref<string[]>([]);
+let currentLine = ref<number>(50);
+let currentDirection = ref<boolean>(false);
+let irreversibleLines = ["50", "52", "62", "100"];
+async function fetchData(lineName: number, direction: boolean = false) {
+    if (irreversibleLines.includes(lineName)) {
+        direction = false;
+    }
     const data = await fetch(
-        `https://rozklady.mzk-konin.com.pl/getDirection.json?lineId=${busLineId[lineName]}&thereDirection=false`
+        `https://rozklady.mzk-konin.com.pl/getDirection.json?lineId=${busLineId[lineName]}&thereDirection=${direction}`
     );
     const dataJson = await data.json();
     return dataJson;
 }
 
-function displayData(lineName: number = 51) {
+function displayData(lineName: number = 51, direction: boolean = false) {
+    if (irreversibleLines.includes(lineName)) {
+        direction = false;
+    }
+    currentLine.value = lineName;
     fetchData(lineName).then((data) => {
-        let stopPointsArr: string[] = [];
+        stopsArr.value = [];
         for (stop of data.stopPoints) {
-            stopPointsArr.push(stop.name);
+            stopsArr.value.push(stop.name);
         }
-        dataGot.value = stopPointsArr.join(", ");
+        if (direction) {
+            stopsArr.value.reverse();
+        }
     });
 }
 
@@ -24,12 +36,21 @@ displayData("50");
 </script>
 
 <template>
-    <select name="" id="" @change="displayData($event.target.value)">
+    <select name="" id="" @change="displayData($event.target.value, false)">
         <option v-for="(line, index) in busLineId" :key="index" :value="index">
             {{ index }}
         </option>
     </select>
-    <p>Przystanki: {{ dataGot }}</p>
+    <button
+        @click="
+            currentDirection = !currentDirection;
+            displayData(currentLine, currentDirection);
+        ">
+        Change direction
+    </button>
+    <ul>
+        <li v-for="stop in stopsArr">{{ stop }}</li>
+    </ul>
 </template>
 
 <style scoped></style>
